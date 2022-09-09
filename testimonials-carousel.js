41dd24f9ed469aa7f5e487b69ca6cb391e0c2267
VuTuanCanh <tuancanhnb@gmail.com>
2022-09-09 11:35:36 +0700

    testimonial completed, skill bars uncompleted

--- testimonials-carousel.js
+++ testimonials-carousel.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.qoute-list').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        dots: true,
     });
 });
